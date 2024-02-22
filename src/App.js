cee590cfedba1a4e25e0bca7d6df3832b02d4593
JuttaVirta_0sfcjaf <JuttaVirta_0sfcjaf@DESKTOP-CJSR0GH>
2024-02-22 15:05:09 +0200

    Added Rotate spinner in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import {useState, useEffect } from 'react';
+import { RotateLoader } from 'react-spinners';
 
 
 function App() {
+
+//Jutta created RotateLoader-spinner to show up if game is loading
+    const [loading, setLoading] = useState(false)
+    useEffect(()=>{
+        setLoading(true)
+        setTimeout(()=>{
+            setLoading(false)
+        }, 3000)
+    }, [])
+
+
   return (
+
+    loading ?
+      <RotateLoader color="#36d7b7"  />
+      :
     <div className="App">
       <header className="App-header">
-        <h1>App header text.</h1>
+        <h1>This text is seen on the website</h1>
       </header>
     </div>
+  
   );
 }
 
