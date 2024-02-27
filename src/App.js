cb8acecdb60d7707ed64b4e9c0cfaa7032e15286
AsravanthiR <sravanthi.avadanula@email.com>
2024-02-27 12:45:47 +0200

    check check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Footer from './Footer';
+
 const App = () => {
   return (
     <div>
       <Footer />
+        <Header/>
+
     </div>
   );
 };
+
 
 export default App;
