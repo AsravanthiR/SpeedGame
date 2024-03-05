c8c05e7fdf22681c2624cf77e5536178d2d955da
JuttaVirta_0sfcjaf <JuttaVirta_0sfcjaf@DESKTOP-CJSR0GH>
2024-03-05 15:31:50 +0200

    created random effect, almost done

--- src/Buttons.js
+++ src/Buttons.js
@@ -5,10 +5,12 @@ function Buttons() {
   const [selectedButton, setSelectedButton] = useState(null);
   const [score, setScore] = useState(0);
 
+
   const getRandomButton = () => {
     const randomIndex = Math.floor(Math.random() * buttonNames.length);
     return buttonNames[randomIndex];
   };
+
 
   const handleButtonClick = (buttonName) => {
     setSelectedButton(buttonName);
@@ -16,16 +18,23 @@ function Buttons() {
   };
 
   return (
-    <div>
+    <div><div className="buttoncontainer">
       {buttonNames.map((buttonName) => (
         <button
           key={buttonName}
           onClick={() => handleButtonClick(buttonName)}
           className={selectedButton === buttonName ? 'selected' : ''}
+          style={getRandomButton() === buttonName ? {border:`20px solid red`}: {}}
+          
         >
+
           {buttonName}
+
         </button>
       ))}
+              <>{getRandomButton() }</>
+
+     </div>
       <p>Score: {score}</p>
     </div>
   );
