7ca2c66e970940c4574ea13fe8dc04112fe9b561
JuttaVirta_0sfcjaf <JuttaVirta_0sfcjaf@DESKTOP-CJSR0GH>
2024-03-05 15:58:15 +0200

    random button selected with red border

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
+//buttonelements array, when selected starting from zero
 function Buttons() {
   const buttonNames = ['LEFT', 'MIDDLE', 'RIGHT'];
   const [selectedButton, setSelectedButton] = useState(null);
   const [score, setScore] = useState(0);
+  const [randomButton, setRandomButton] = useState(null);
 
-
+  //randomly choosing one button from the array and rounding the desimal
   const getRandomButton = () => {
     const randomIndex = Math.floor(Math.random() * buttonNames.length);
     return buttonNames[randomIndex];
   };
 
-
   const handleButtonClick = (buttonName) => {
     setSelectedButton(buttonName);
     setScore((prevScore) => prevScore + 5);
   };
+
+  
+  setInterval(() => {
+    setRandomButton (getRandomButton())
+}, 3000);
+ 
 
   return (
     <div><div className="buttoncontainer">
@@ -24,15 +31,13 @@ function Buttons() {
           key={buttonName}
           onClick={() => handleButtonClick(buttonName)}
           className={selectedButton === buttonName ? 'selected' : ''}
-          style={getRandomButton() === buttonName ? {border:`20px solid red`}: {}}
-          
+          style={randomButton === buttonName ? {border:`20px solid red`}: {}}
         >
-
           {buttonName}
 
         </button>
       ))}
-              <>{getRandomButton() }</>
+              <>{randomButton }</>
 
      </div>
       <p>Score: {score}</p>
